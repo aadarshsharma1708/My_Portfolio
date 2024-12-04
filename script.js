c554516d3a8c79d4c0d5d6f0bc07633cc0733d9d
Adarsh Sharma <166322445+aadarshsharma1708@users.noreply.github.com>
2024-12-04 21:29:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu=document.querySelector(".hamburger-link");
+    const icon=document.querySelector(".hamburger-icons");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}   
